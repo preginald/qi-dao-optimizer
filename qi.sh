b83794dae71f8b198eff44b98d5fcc9d1036fd56
preginald <peter@reginald.com.au>
2022-03-06 22:27:54 +1100

    add cxDOGE and remove camWETH and camWBTC

--- qi.sh
+++ qi.sh
@@ -21,11 +21,11 @@ else
     acc_id=$1
 fi
 
-if [[ "$2" =~ ^(cwb|cwe|cwm)$ ]]; then
+if [[ "$2" =~ ^(cwm|cxd)$ ]]; then
     vault_asset=$2
 else
     echo "Select which vault"
-    echo -n "cwm = camWMATIC, cwe = camWETH, cwb = camWBTC: "
+    echo -n "cwm = camWMATIC, cxd = cxDOGE: "
     read vault_asset
 fi
 
@@ -43,7 +43,7 @@ else
     network_id=$4
 fi
 
-declare -A functions=( ["cwm"]="camWMATIC" ["cwe"]="camWETH")
+declare -A functions=( ["cwm"]="camWMATIC" ["cxd"]="cxDOGE" )
 
 function=${functions[$vault_asset]}
 
