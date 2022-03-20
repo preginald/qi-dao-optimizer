385bf77f614b5a3d9f513ad70e64cb2e42a09623
preginald <peter@reginald.com.au>
2022-03-20 16:15:18 +1100

    add support for vGHST Mai vault

--- qi.sh
+++ qi.sh
@@ -29,11 +29,11 @@ else
     acc_id=$1
 fi
 
-if [[ "$2" =~ ^(cwm|cxd)$ ]]; then
+if [[ "$2" =~ ^(cwm|cxd|vgm)$ ]]; then
     vault_asset=$2
 else
     echo "Select which vault"
-    echo -n "cwm = camWMATIC, cxd = cxDOGE: "
+    echo -n "cwm = camWMATIC, cxd = cxDOGE, vgm = vGMVT: "
     read vault_asset
 fi
 
@@ -51,7 +51,7 @@ else
     network_id=$4
 fi
 
-declare -A functions=( ["cwm"]="camWMATIC" ["cxd"]="cxDMVT" )
+declare -A functions=( ["cwm"]="camWMATIC" ["cxd"]="cxDMVT" ["vgm"]="vGMVT")
 
 function=${functions[$vault_asset]}
 
